3d8bf2209bdacd9ba7245faf1659979797956153
alexsaraujo1 <82852965+alexsaraujo1@users.noreply.github.com>
2023-05-24 22:01:32 -0300

    init

---
+++ springboot-backend/src/main/java/com/t3/springbootbackend/entity/User.java
@@ -0,0 +1,34 @@
+package com.t3.springbootbackend.entity;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+}
